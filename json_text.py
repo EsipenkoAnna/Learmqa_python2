c9aeb4da375f0148a306ce76b6e56df4deb06021
EsipenkoAnna <annette160893@mail.ru>
2023-11-16 23:36:14 +0300

    1

---
+++ json_text.py
@@ -0,0 +1,3 @@
+json_text = {"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}
+print(json_text["messages"][1]['message'])
+print(type(json_text))
